e25a43e534394d28e9a8adf250ccf9c7c506757a
Jafar-shaik <skjafar4944@gmail.com>
2025-07-08 21:40:14 +0530

    Initial commit

---
+++ src/test/java/com/jafar/expensesplitterapp/ExpenseSplitterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafar.expensesplitterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplitterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
